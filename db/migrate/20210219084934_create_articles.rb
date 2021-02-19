5bf2864fb211f20ecfda197aaf8c63650992194b
shantanu-sj <1990shantanu@gmail.com>
2021-02-19 14:40:38 +0530

    added article migrations

--- db/migrate/20210219084934_create_articles.rb
+++ db/migrate/20210219084934_create_articles.rb
@@ -2,9 +2,7 @@ class CreateArticles < ActiveRecord::Migration[6.1]
   def change
     create_table :articles do |t|
       t.string :title
-      t.text :description
-
-      t.timestamps
+      t.text :decription
     end
   end
 end
